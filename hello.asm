536f3595f234b20633d484ac38d9d91346f9e9ad
Neil Ramsbottom <nramsbottom@hotmail.com>
2023-08-30 18:19:24 +0200

    Small fixes.
    * The message is now shown before waiting for a key.
    * DosBox will automatically exit when the app exits.

--- hello.asm
+++ hello.asm
@@ -7,20 +7,21 @@ mov ah, 00h
 mov al, 13h
 int 10h
 
-wait_for_key:
-; read key, no echo
-mov ah, 08h
-int 21h
-
+show_message:
 ; show the message
 mov dx, msg
 mov ah, 09h
 int 21h
 
+wait_for_key:
+; read key, no echo
+mov ah, 08h
+int 21h
+
 ; press 'x' to quit
 cmp al, 78h
 je exit
-jmp wait_for_key
+jmp show_message
 
 
 
